b0b6d426e00ba0a5b72ef358132c3c9c8469167c
boreddad <ronnebaumfinances@gmail.com>
2023-06-22 10:21:46 -0400

    fix: change hard coded url to window.location.origin

--- apps/stock/app/src/api.ts
+++ apps/stock/app/src/api.ts
@@ -9,28 +9,28 @@ export default class Api {
     }
 
     public static async fundamentals(symbol: string): Promise<Fundamentals> {
-        let url = `http://localhost:42069/api/fundamentals/${symbol}`;
+        let url = window.location.origin + `/api/fundamentals/${symbol}`;
         let res = await fetch(url);
         let t = await res.json() as Fundamentals;
         return t;
     }
 
     public static async income(symbol: string): Promise<Income> {
-        let url = `http://localhost:42069/api/income/${symbol}`;
+        let url = window.location.origin + `/api/income/${symbol}`;
         let res = await fetch(url);
         let r = await res.json() as Income;
         return r;
     }
 
     public static async balance(symbol: string): Promise<Balance> {
-        let url = `http://localhost:42069/api/balance/${symbol}`;
+        let url = window.location.origin + `/api/balance/${symbol}`;
         let res = await fetch(url);
         let r = await res.json() as Balance;
         return r;
     }
 
     public static async cashflow(symbol: string): Promise<CashFlow> {
-        let url = `http://localhost:42069/api/cash/${symbol}`;
+        let url = window.location.origin + `/api/cash/${symbol}`;
         let res = await fetch(url);
         let r = await res.json() as CashFlow;
         return r;
