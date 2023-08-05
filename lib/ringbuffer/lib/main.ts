eaccc093890002879cd104773b19323f2d07b72f
boreddad <ronnebaumfinances@gmail.com>
2023-08-05 08:16:54 -0400

    clean 2

--- lib/ringbuffer/lib/main.ts
+++
@@ -1,5 +0,0 @@
-import RingBuffer from "./ringbuffer";
-
-export default function ringBuffer<T>(initialCap: number, factory: () => T): RingBuffer<T> {
-    return new RingBuffer(initialCap, factory);
-}
