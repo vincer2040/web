99e9b5167e38d7f1a436d45ed5bcfb9bc0717aa3
boreddad <ronnebaumfinances@gmail.com>
2023-10-05 16:26:14 -0400

    feat: (stock) balance sheet

--- apps/stock/internal/api/api.go
+++ apps/stock/internal/api/api.go
@@ -10,8 +10,8 @@ import (
 
 func GetOverview(company string, apikey string) (*Overview, error) {
 	url := fmt.Sprintf(
-        "https://www.alphavantage.co/query?function=OVERVIEW&symbol=%s&apikey=%s",
-        strings.ToUpper(company), apikey)
+		"https://www.alphavantage.co/query?function=OVERVIEW&symbol=%s&apikey=%s",
+		strings.ToUpper(company), apikey)
 	var overview Overview
 
 	response, err := http.Get(url)
@@ -36,8 +36,8 @@ func GetOverview(company string, apikey string) (*Overview, error) {
 
 func GetIncomeStatements(company string, apikey string) (*IncomeStatements, error) {
 	url := fmt.Sprintf(
-        "https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=%s&apikey=%s",
-        strings.ToUpper(company), apikey)
+		"https://www.alphavantage.co/query?function=INCOME_STATEMENT&symbol=%s&apikey=%s",
+		strings.ToUpper(company), apikey)
 	var incomeStatements IncomeStatements
 
 	response, err := http.Get(url)
@@ -58,4 +58,30 @@ func GetIncomeStatements(company string, apikey string) (*IncomeStatements, erro
 	}
 
 	return &incomeStatements, nil
+}
+
+func GetBalanceSheet(company string, apikey string) (*BalanceSheet, error) {
+	url := fmt.Sprintf(
+		"https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=%s&apikey=%s",
+		strings.ToUpper(company), apikey)
+	var balanceSheet BalanceSheet
+
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &balanceSheet)
+	if err != nil {
+		return nil, err
+	}
+
+	return &balanceSheet, nil
 }
